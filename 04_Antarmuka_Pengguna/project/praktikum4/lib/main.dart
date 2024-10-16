aadd0d7ac12ef43f9a5b2e0e813d47d9a6fbefeb
Mohammad Dhimas Afrizal <mdhimas25@gmail.com>
2024-10-16 19:01:02 +0700

    update

--- 04_Antarmuka_Pengguna/project/praktikum4/lib/main.dart
+++ 04_Antarmuka_Pengguna/project/praktikum4/lib/main.dart
@@ -1,93 +1,63 @@
+
 import 'package:flutter/material.dart';
-
-class ListViewApp extends StatelessWidget {
-  const ListViewApp({super.key});
-
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Rekomendasi Wisata',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 97, 61, 176)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Recommended Tourist Places'),
-    );
-  }
+      home: const MyHomePage(
+        title: 'Rekomendasi Wisata',
+        ),
+); }
 }
-
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final List<Map<String, String>> places = [
-    {
-      'name': 'Eiffel Tower',
-      'location': 'Paris, France',
-      'image':
-          'https://cdn1.sisiplus.co.id/media/sisiplus/asset/uploads/artikel/g2OEbKl2aTEIp1x5hFNYEE9ad615uVenBexDAcVW.jpg'
-    },
-    {
-      'name': 'Great Wall of China',
-      'location': 'China',
-      'image':
-          'https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1638892506/EducationHub/photos/the-great-wall-of-china.jpg'
-    },
-    {
-      'name': 'Machu Picchu',
-      'location': 'Peru',
-      'image':
-          'https://asset.kompas.com/crops/-0kUbM44VWEdaGoXSfe5xc3yRTA=/0x0:739x493/750x500/data/photo/2020/10/14/5f86abb8005a4.jpg'
-    },
-    {
-      'name': 'Grand Canyon',
-      'location': 'Arizona, USA',
-      'image':
-          'https://upload.wikimedia.org/wikipedia/commons/3/31/Canyon_River_Tree_%28165872763%29.jpeg'
-    },
-    {
-      'name': 'Colosseum',
-      'location': 'Rome, Italy',
-      'image':
-          'https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1652340658/EducationHub/photos/colosseum.jpg'
-    },
-  ];
-
-  @override
-  Widget build(BuildContext context) {
+const MyHomePage({super.key, required this.title}); final String title;
+@override
+State<MyHomePage> createState() => _MyHomePageState(); }
+class _MyHomePageState extends State<MyHomePage> { @override
+Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        centerTitle: true,
       ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemCount: places.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: Column(
-              children: [
-                // Displaying image from URL
-                Image.network(
-                  places[index]['image']!,
-                  height: 200,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-                ListTile(
-                  title: Text(places[index]['name']!),
-                  subtitle: Text(places[index]['location']!),
-                ),
-              ],
+      body: Center(
+        child: Column(
+mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            const Text(
+              "Baturaden",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
             ),
-          );
-        },
-      ),
-    );
-  }
-}
+            Image.network('https://cdn.paradisotour.co.id/wp-content/uploads/2024/09/Cagar-Alam-Baturaden.jpg',
+              width: 300,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+   
+ child: Text(
+"Baturaden adalah sebuah objek wisata alam yang terletak di lereng Gunung Slamet, sekitar 15 kilometer dari pusat kota Purwokerto, Kabupaten Banyumas, Jawa Tengah. Terkenal dengan pemandangan alamnya yang indah dan udara yang sejuk, Baturaden menjadi destinasi favorit wisatawan lokal maupun mancanegara.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+), ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {},
+child: const Text('Kunjungi Sekarang'), ),
+], ),
+), );
+} }
+  
--- 04_Antarmuka_Pengguna/project/praktikum4/lib/tugas1.dart
+++ 04_Antarmuka_Pengguna/project/praktikum4/lib/tugas1.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const ListViewApp());
+}
+
 class ListViewApp extends StatelessWidget {
   const ListViewApp({super.key});
 
